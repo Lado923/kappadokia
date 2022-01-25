8ad4f92cb4ca4e45a60107d7616df2ccb4a63b64
Lado923 <slava923@gmail.com>
2022-01-25 20:46:48 +0300

    feat:add second slider

--- js/main.js
+++ js/main.js
@@ -41,6 +41,7 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";    
 }
 
+/*---AUTOPLAY---FIRST SLIDER---*/
 var timeInterval = 3000,
     automate = true,
     timerID = null;
@@ -54,4 +55,67 @@ if (automate) {
         slideIndex = 0;
     }
    }, timeInterval);
+}
+
+
+/*---------------------------------------------------------*/
+/*----SECOND SLIDER----*/
+/* Устанавливаем стартовый индекс слайда по умолчанию: */
+let slideIndex2 = 1;
+/* Вызываем функцию, которая реализована ниже: */
+showSlides2(slideIndex2);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide2() {
+    showSlides2(slideIndex2 += 1);
+}
+
+function previousSlide2() {
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+    showSlides2(slideIndex2 -= 1);  
+}
+
+/* Устанавливаем текущий слайд: */
+function currentSlide2(m) {
+    showSlides2(slideIndex2 = m);
+}
+
+
+
+/* Функция перелистывания: */
+function showSlides2(m) {
+    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
+    let slides2 = document.getElementsByClassName("our-trip__slider-item");
+    
+    /* Проверяем количество слайдов: */
+    if (m > slides2.length) {
+      slideIndex2 = 1
+    }
+    if (m < 1) {
+        slideIndex2 = slides2.length
+    }
+  
+    /* Проходим по каждому слайду в цикле for: */
+    for (let slide2 of slides2) {
+        slide2.style.display = "none";
+    }
+    /* Делаем элемент блочным: */
+    slides2[slideIndex2 - 1].style.display = "block";    
+}
+
+
+/*---AUTOPLAY---SECOND SLIDER---*/
+var timeInterval2 = 3000,
+    automate2 = true,
+    timerID2 = null;
+
+if (automate2) {
+   timerID2 = setInterval(function() {
+    let slides2 = document.getElementsByClassName("our-trip__slider-item");
+    slideIndex2++;
+    showSlides2();
+    if (slideIndex2 == slides2.length) {
+        slideIndex2 = 0;
+    }
+   }, timeInterval2);
 }
